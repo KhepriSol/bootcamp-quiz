1a6af50f8a92fc7bc6b4fbdeb979c271380263a8
KhepriSol <118213369+KhepriSol@users.noreply.github.com>
2023-01-09 16:27:31 -0600

    working on start button functionality
    trying to make my timer work, when the start button is pressed

--- dist/function.dev.js
+++ dist/function.dev.js
@@ -2,8 +2,33 @@
 
 var form = document.getElementById(quiz);
 var startButton = document.getElementById("start-button");
-startButton.addEventListener("click", function () {// start the quiz
+startButton.addEventListener("click", function () {
+  // start quiz and timeclock
+  var timeLeft = 60; //time in seconds
+
+  var timerInterval = setInterval(function () {
+    timeLeft--;
+    updateTimer(timeLeft); //update the timeclock on screen display
+
+    if (timeLeft === 0) {
+      clearInterval(timerInterval);
+      endQuiz();
+    }
+  }, 1000); //update timer every second 
+  //show the first question
+
+  showQuestion(1);
 });
+
+function showQuestion(questionNumber) {// show the question and options
+}
+
+function updateTimer(timeLeft) {// update the timer display
+}
+
+function endQuiz() {// end the quiz and show the form for saving the score
+}
+
 form.addEventListener("submit", function (event) {
   event.preventDefault(); //Prevents the form from submitting//
   //selected options for each question//
