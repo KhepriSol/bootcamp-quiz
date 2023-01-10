d367c72e106ed197b160e47bea2d4a4f3e460fec
KhepriSol <118213369+KhepriSol@users.noreply.github.com>
2023-01-09 20:12:30 -0600

    deleted js
    deleted the js after reworking html

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -4,66 +4,8 @@ var form = document.getElementById("Pokemon");
 var startButton = document.getElementById("start-button");
 var questionsContainer = document.getElementById("questions");
 var gameOverForm = document.getElementById("game-over");
-var timeLeft = 60; // time in seconds
-
-var timerInterval;
-
-function updateTimer(timeLeft) {
-  // update the timer display
-  var timer = document.getElementById("timer");
-  timer.textContent = timeLeft;
-}
-
-startButton.addEventListener("click", function () {
-  // start quiz and timer
-  timerInterval = setInterval(function () {
-    timeLeft--;
-    updateTimer(timeLeft); // update the timer display
-
-    if (timeLeft === 0) {
-      clearInterval(timerInterval);
-      endQuiz();
-    }
-  }, 1000); // update timer every second
-  // show the first question
-
-  showQuestion(1);
-});
-
-function showQuestion(questionNumber) {
-  // show the question and options
-  currentQuestion = questionNumber;
-}
-
-function endQuiz() {
-  // end the quiz and show the form for saving the score
-  form.style.display = "block";
-}
-
-var currentQuestion = 1;
-form.addEventListener("submit", function (event) {
-  event.preventDefault(); // Preventing the form from submitting
-  // selected options for each question
-
-  var question1 = form.elements.question1;
-  var question2 = form.elements.question2; // add more pokemon questions etc
-  // check if the selected options are correct
-
-  if (question1.value === "A") {
-    // correct answer
-    showQuestion(2);
-  } else {
-    // incorrect answer
-    timeLeft -= 10;
-  }
-
-  if (question2.value === "A") {
-    // correct answer
-    showQuestion(3);
-  } else {
-    // incorrect answer
-    timeLeft -= 10;
-  } // more questions
-
-});
+var timer;
+var questions;
+startButton.addEventListener("click", startGame);
+gameOverForm.addEventListener("submit", saveScore);
 //# sourceMappingURL=script.dev.js.map
--- script.js
+++ script.js
@@ -3,73 +3,10 @@ const startButton = document.getElementById("start-button");
 const questionsContainer = document.getElementById("questions");
 const gameOverForm = document.getElementById("game-over");
 
-
-let timeLeft = 60; // time in seconds
-let timerInterval;
-
-function updateTimer(timeLeft) {
-  // update the timer display
-  const timer = document.getElementById("timer");
-  timer.textContent = timeLeft;
-}
-
-startButton.addEventListener("click", function() {
-  // start quiz and timer
-  timerInterval = setInterval(function() {
-    timeLeft--;
-    updateTimer(timeLeft); // update the timer display
-    if (timeLeft === 0) {
-      clearInterval(timerInterval);
-      endQuiz();
-    }
-  }, 1000); // update timer every second
-
-  // show the first question
-  showQuestion(1);
-});
-
-function showQuestion(questionNumber) {
-  // show the question and options
-  currentQuestion = questionNumber;
-}
-
-function endQuiz() {
-  // end the quiz and show the form for saving the score
-  form.style.display = "block";
-}
-
-let currentQuestion = 1;
-
-form.addEventListener("submit", function(event) {
-  event.preventDefault(); // Preventing the form from submitting
-
-  // selected options for each question
-  const question1 = form.elements.question1;
-  const question2 = form.elements.question2;
-  // add more pokemon questions etc
-
-  // check if the selected options are correct
-  if (question1.value === "A") {
-    // correct answer
-    showQuestion(2);
-  } else {
-    // incorrect answer
-    timeLeft-=10;
-  }
-  if (question2.value === "A") {
-    // correct answer
-    showQuestion(3);
-  } else {
-    // incorrect answer
-    timeLeft-=10;
-  }
-  // more questions
-});
+let timer;
+let questions;
 
 
-
-
-
-
-
+startButton.addEventListener("click", startGame);
+gameOverForm.addEventListener("submit", saveScore);
 
